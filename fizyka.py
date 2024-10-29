c434809a026d61fb008997453543a37d169c350c
Jakub Pietras <Kubapietras4002@wp.pl>
2024-10-29 13:23:22 +0100

    Refactored

--- fizyka.py
+++ fizyka.py
@@ -1,65 +1,57 @@
 from matplotlib.animation import FuncAnimation
 import numpy as np
 import matplotlib.pyplot as plt
+
 np.set_printoptions(suppress=True)
 
-class  Czastka:
-    A:str
-    B:str
-    points = np.empty((0, 2), dtype=float)
+class Czastka:
     def __init__(self, A, B):
         self.A = A
         self.B = B
+        self.points = np.empty((0, 2), dtype=float)
         
-    def return_A(self):
-        return self.A
-    
-    def return_B(self):
-        return self.B
-    
-    def return_points(self):
-        return self.points
-    
     def append_points(self, x, y):
         self.points = np.append(self.points, [[x, y]], axis=0)
-        
-set_interval = int(input("Podaj co ile ma sie zmieniac klatka (ms):  "))    
-czastki = []
-rownania = int(input("Podaj ilość równań: "))
-for i in range(rownania):
-    A_part = input("Podaj wartość A: ")
-    B_part = input("Podaj wartość B: ")
-    czastki.append(Czastka(A_part, B_part))
-    
-points_limit = int(input("Podaj ostatna wartosc 't' do obliczenia: "))
-step = float(input("Podaj co ile ma obliczac: "))
-calc = 0
-points = np.empty((0, 2), dtype=float)
-maxcalc = 0
-for czastka in czastki:
-    calc = 0
-    while calc < points_limit+1:
-        x = eval(czastka.return_A()   , {'t': calc})
-        y = eval(czastka.return_B(), {'t': calc})
-        calc += step
-        czastka.append_points(x, y)
 
-fig, ax = plt.subplots()
-scatters = [ax.plot([], [], 'o', label=f'Czastka {i+1}')[0] for i in range(len(czastki))]
+def get_user_input():
+    set_interval = int(input("Podaj co ile ma sie zmieniac klatka (ms): "))
+    rownania = int(input("Podaj ilość równań: "))
+    czastki = [Czastka(input("Podaj wartość A: "), input("Podaj wartość B: ")) for _ in range(rownania)]
+    points_limit = int(input("Podaj ostatna wartosc 't' do obliczenia: "))
+    step = float(input("Podaj co ile ma obliczac: "))
+    return set_interval, czastki, points_limit, step
 
-def init():
-    ax.set_xlim(0, step*max([eval(czastka.return_A(), {'t': points_limit}) for czastka in czastki]))
-    ax.set_ylim(0, step*max([eval(czastka.return_B(), {'t': points_limit}) for czastka in czastki]))
-    return scatters
+def calculate_points(czastki, points_limit, step):
+    for czastka in czastki:
+        calc = 0
+        while calc <= points_limit:
+            x = eval(czastka.A, {'t': calc})
+            y = eval(czastka.B, {'t': calc})
+            czastka.append_points(x, y)
+            calc += step
 
-def update(frame):
+def init_plot(czastki, points_limit, step):
+    fig, ax = plt.subplots()
+    scatters = [ax.plot([], [], 'o', label=f'Czastka {i+1}')[0] for i in range(len(czastki))]
+    ax.set_xlim(0, step * max(eval(czastka.A, {'t': points_limit}) for czastka in czastki))
+    ax.set_ylim(0, step * max(eval(czastka.B, {'t': points_limit}) for czastka in czastki))
+    plt.xlabel('x[m]')
+    plt.ylabel('y[m]')
+    plt.legend()
+    return fig, ax, scatters
+
+def update(frame, scatters, czastki):
     for scatter, czastka in zip(scatters, czastki):
-        points = czastka.return_points()
+        points = czastka.points
         scatter.set_data(points[frame, 0], points[frame, 1])
     return scatters
 
-ani = FuncAnimation(fig, update, frames=range(points_limit+1), init_func=init, blit=True, interval=set_interval, repeat=True)
-plt.xlabel('x[m]')
-plt.ylabel('y[m]')
-plt.legend()
-plt.show()
+def main():
+    set_interval, czastki, points_limit, step = get_user_input()
+    calculate_points(czastki, points_limit, step)
+    fig, ax, scatters = init_plot(czastki, points_limit, step)
+    ani = FuncAnimation(fig, update, frames=range(points_limit + 1), fargs=(scatters, czastki), init_func=lambda: scatters, blit=True, interval=set_interval, repeat=True)
+    plt.show()
+
+if __name__ == "__main__":
+    main()
